8105169c11a8e3a87f8a2044de3c4a25aba3292b
MeghnaSedhain <meghnasedhain1@gmail.com>
2026-06-24 11:01:42 +0545

    auth JWT

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import { signup, login, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
