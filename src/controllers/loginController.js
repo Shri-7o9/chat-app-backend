570d31bbfdadd6e65b0b2cdb730f20e7a4125785
pramod panta <pantapramod20260@gmail.com>
2026-07-01 14:57:26 +0545

    login controller

---
+++ src/controllers/loginController.js
@@ -0,0 +1,77 @@
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import { generateToken } from "../libs/utils.js";
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Validate input
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    // 2. Check if user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // 3. Compare password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // 4. Generate JWT token
+    const token = generateToken(user._id);
+
+    // 5. Send response
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+    
+      },
+    });
+  } catch (error) {
+    console.log("Login error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+//logout controller
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).json({
+      success: true,
+      message: "Logout successful",
+    });
+  } catch (error) {
+    console.log("Logout error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};	
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,17 +1,14 @@
 import express from "express";
 
 import {
-registerUser,
-loginUser,
-verifyEmail
+login, 
+logout
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register",registerUser);
-
-router.post("/login",loginUser);
-
-router.post("/verify-email",verifyEmail);
+router.post("/login", login); 
+router.post("/logout", logout);   
 
 export default router;
+
