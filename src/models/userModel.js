6ff77c6d7ae4e5c74dfe2e2f65f021c215f9e316
supriya <supriyasapkota276@gmail.com>
2026-07-01 12:01:31 +0545

    Update user model validation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-{
-firstName:{type:String,required:true},
-lastName:{type:String,required:true},
-email:{type:String,required:true},
-password:{type:String,required:true}
-
-},
-{timestamps:true}
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+  },
+  { timestamps: true },
 );
-
-
 
 const User = mongoose.model("User", userSchema);
 
