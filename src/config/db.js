e54ba7c6a135b60a90424c4111121432d6d8c88f
Shri <shrivatsa709@gmail.com>
2026-07-01 14:09:04 +0545

    fixedd db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI:", process.env.MONGODB_URI);
 
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("MongoDB connected:", conn.connection.host);
+    console.log("MongoDB connected");
   } catch (error) {
     console.log("DB connection error:", error.message);
     process.exit(1);
