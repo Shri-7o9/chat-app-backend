2c5e434b443561b6d57937fea4f54e7ffda052a2
Shri <shrivatsa709@gmail.com>
2026-07-02 07:54:37 +0545

    Added CORS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import messageRoutes from "./routes/messageRoutes.js";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import { connectDB } from "./libs/db.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -12,6 +13,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+)
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
