1e0a6563825f677b616925ff3dee85b077e19453
MeghnaSedhain <meghnasedhain1@gmail.com>
2026-07-03 10:28:56 +0545

    messagecontroller

---
+++ src/controllers/messageController.js
@@ -0,0 +1,62 @@
+import User from "../models/userModel.js";
+import Message from "../models/messageModel.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.userId;
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.userId;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.userId;
+
+    let imageUrl;
+    if (image) {
+  
+      imageUrl = image; 
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in sendMessage: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
