5851231c9385886eccf39d194de9f1d5d54a4c81
supriya <supriyasapkota276@gmail.com>
2026-07-08 11:02:56 +0545

    Add authRoute

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,14 +1,27 @@
 import express from "express";
 
 import { signup } from "../controllers/signupController.js";
-import { login } from "../controllers/loginController.js";
+import { loginUser } from "../controllers/loginController.js";
 import { logoutUser } from "../controllers/logoutController.js";
+import { updateUser } from "../controllers/updateController.js";
+
+import { verifyEmail } from "../controllers/authController.js";
+import { forgotPassword } from "../controllers/forgotPasswordController.js";
+import { resetPassword } from "../controllers/resetPasswordController.js";
+
 import { protectRoute } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/signup", signup);
+router.post("/login", loginUser);
+
 router.post("/logout", protectRoute, logoutUser);
-router.post("/register", signup);
+router.put("/update", protectRoute, updateUser);
+
+router.get("/verify/:token", verifyEmail);
+
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword/:token", resetPassword);
 
 export default router;
