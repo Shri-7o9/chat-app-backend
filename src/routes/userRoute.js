f6eaf1115040c367c91fee0c6f5d7bbaade6358b
Shri <148345258+Shri-7o9@users.noreply.github.com>
2026-07-14 11:23:22 +0545

    Delete src/routes/userRoute.js

--- src/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { updateUser } from "../controllers/updateController.js";
-import { upload } from "../middleware/uploadMiddleware.js";
-import { protectRoute } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-export default router;
