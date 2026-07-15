39321907efe1ae94ec98f2c63b6b86a5cd04e7c4
Shri <shrivatsa709@gmail.com>
2026-07-15 11:16:33 +0545

    Revert "Update index.js"
    This reverts commit cfe445b8d7fa41573361e242fc5f93cde4d203b5.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
@@ -16,21 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    })
-)
 
 app.use("/api/auth",authRoute);
 app.use("/api/messages", messageRoutes);
 
 app.use("/api/user", userRoutes); //added
 
-connectDB().then(() => {
-  const PORT = process.env.PORT || 5001;
-
-  app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-  });
+app.listen(process.env.PORT, () => {
+    connectDB();
+  console.log(`Server running on port ${process.env.PORT}`);
+});
